f34b7da352fc3f4c3d07e31f34e4af1b7a56c4e0
Farai Major <faraimutukumira@gmail.com>
2023-01-15 02:00:45 -0500

    add food and update scores

--- day20/main.py
+++ day20/main.py
@@ -1,14 +1,20 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("green")
 screen.title("Classic Snake Game")
 screen.tracer(0)
 
-
 snake = Snake()
+food = Food()
+score = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,5 +28,10 @@ while game_is_on:
     time.sleep(0.2)
 
     snake.move()
+
+    # detect collision with food using distance()
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        score.increase_score()
 
 screen.exitonclick()
