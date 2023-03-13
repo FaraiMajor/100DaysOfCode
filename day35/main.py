f1526da3fc2f675909d34f0e8277e39ef60e41e2
Farai Major <faraimutukumira@gmail.com>
2023-03-13 00:36:08 -0400

    open weather map

---
+++ day35/main.py
@@ -0,0 +1,28 @@
+import requests
+
+
+api_key = "7df7c4df02edd6021c39f04315142c70"
+url = "https://api.openweathermap.org/data/2.5/onecall"
+
+params = {
+    "lat": 40.679459,
+    "lon": -73.846413,
+    "appid": api_key,
+    "units": "imperial",
+    "exclude": "current,minute,daily"
+}
+response = requests.request("GET", url, params=params)
+response.raise_for_status()
+
+weather_data = response.json()
+weather_hourly = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hourly_data in weather_hourly:
+    condition_code = hourly_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("It's going to rain today. Remember to bring an ☔️")
